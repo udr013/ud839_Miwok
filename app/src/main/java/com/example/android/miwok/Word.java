9a3baf95d51f893f776ac56b431c1c6902ce97e6
udr013 <udr013@gmail.com>
2016-08-19 21:26:05 +0200

    added images and added imageResourceId to Word class

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,12 +4,27 @@ package com.example.android.miwok;
  * Created by udr013 on 19-8-2016.
  */
 public class Word {
+    private int  imageResourceId;
     private String defaultWord;
     private String miwokWord;
 
     public Word(String defaultWord, String miwokWord) {
         this.defaultWord = defaultWord;
        this.miwokWord = miwokWord;
+    }
+
+    public Word(int imageResourceId, String defaultWord, String miwokWord) {
+        this.imageResourceId = imageResourceId;
+        this.defaultWord = defaultWord;
+        this.miwokWord = miwokWord;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public void setImageResourceId(int imageResourceId) {
+        this.imageResourceId = imageResourceId;
     }
 
     public String getDefaultWord() {
