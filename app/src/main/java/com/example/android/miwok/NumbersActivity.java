dd04a0c1c168679d42dce22585619c0da8857470
udr013 <udr013@gmail.com>
2016-08-19 13:38:58 +0200

    added list of TextViews to NumberActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+//        String[] numbers = new String[10];
+        ArrayList<String> numbers = new ArrayList<>();
+        numbers.add("One");
+        numbers.add("Two");
+        numbers.add("Three");
+        numbers.add("Four");
+        numbers.add("Five");
+        numbers.add("Six");
+        numbers.add("Seven");
+        numbers.add("Eight");
+        numbers.add("Nine");
+        numbers.add("Ten");
+
+        LinearLayout numbersView = (LinearLayout) findViewById(R.id.numbersView);
+
+        for (String nr : numbers) {
+            // the "this" keyword refers to the current class as  the context (numbersActivity)
+            TextView textView = new TextView(this);
+            textView.setText(nr);
+            numbersView.addView(textView);
+            Log.v("numbers Activity", "numberArray contains: " + nr);
+        }
     }
 }
