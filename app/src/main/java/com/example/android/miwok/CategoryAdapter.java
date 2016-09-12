b3c17e63b5a7f71159e39b6f1b20e15b4d7b2b7b
udr013 <udr013@gmail.com>
2016-09-12 14:11:43 +0200

    added Tablayout

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -8,9 +9,12 @@ import android.support.v4.app.FragmentPagerAdapter;
  * Created by udr013 on 12-9-2016.
  */
 public class CategoryAdapter extends FragmentPagerAdapter {
+    private String tabTitles[] = new String[] { "Tab1", "Tab2", "Tab3","Tab4" };
+    private Context context;
 
-    public CategoryAdapter(FragmentManager fm) {
+    public CategoryAdapter(FragmentManager fm, Context context) {
         super(fm);
+        this.context = context;
     }
 
     @Override
@@ -28,5 +32,16 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if(position ==0){
+            return context.getString(R.string.category_numbers);
+        }else if (position == 1){
+            return context.getString(R.string.category_colors);
+        }else if (position == 2){
+            return  context.getString(R.string.category_family);
+        }else return context.getString(R.string.category_phrases);
     }
 }
